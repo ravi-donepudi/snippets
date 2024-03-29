a71e523877424de170cc28e13a185c9c63e4c86d
Ravi Donepudi <ravidonepudi.phd@gmail.com>
2024-03-29 11:58:10 -0500

    clean up unused lines

--- ocr/tesseract-test.py
+++ ocr/tesseract-test.py
@@ -1,9 +1,6 @@
 from PIL import Image
 import pytesseract
-import pandas as pd
 
-# df = pd.read_csv(r"C:\Users\iluvm\Desktop\github\snippets\ocr\data\test_text.csv")
-# print(df)
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
