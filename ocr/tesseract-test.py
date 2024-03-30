447e0a64dc5b90da6db5e8d3a610363d61c03475
Ravi Donepudi <ravidonepudi.phd@gmail.com>
2024-03-30 12:57:39 -0500

    clean up formatting

--- ocr/tesseract-test.py
+++ ocr/tesseract-test.py
@@ -1,7 +1,6 @@
 from PIL import Image
 import pytesseract
 
-
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 print(pytesseract.image_to_string(Image.open(r"C:\Users\iluvm\Desktop\github\snippets\ocr\data\simple_table.png")))
